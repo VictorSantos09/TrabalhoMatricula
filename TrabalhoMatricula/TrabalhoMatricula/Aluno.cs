1418b676ff3aef95e85c0ac75d88b0e054c2c6c6
VictorSantos09 <110687512+VictorSantos09@users.noreply.github.com>
2022-09-17 21:42:31 -0300

    Add files via upload

---
+++ TrabalhoMatricula/TrabalhoMatricula/Aluno.cs
@@ -0,0 +1,54 @@
+﻿namespace TrabalhoMatricula
+{
+    public class Aluno
+    {
+        public string Name { get; set; }
+        public string MotherName { get; set; }
+        public string BornState { get; set; }
+        public string ActualState { get; set; }
+        public string DateBorn { get; set; }
+        public string Phone { get; set; }
+        public string Adress { get; set; }
+        public int Process { get; set; }
+        public void StudentRegister(int contador)
+        {
+            Console.Write($"Digite o nome do aluno {contador}: ");
+            Name = Console.ReadLine();
+            Console.Write($"Digite o nome da mãe do aluno {contador}: ");
+            MotherName = Console.ReadLine();
+            Console.Write($"Digite o estado no qual o aluno {contador} nasceu: ");
+            BornState = Console.ReadLine();
+            Console.Write($"Digite o estado atual em que o aluno {contador} reside: ");
+            ActualState = Console.ReadLine();
+            Console.Write($"Digite a data de nascimento do aluno {contador}: ");
+            DateBorn = Console.ReadLine();
+            Console.Write($"Digite o telefone do aluno {contador}: ");
+            Phone = Console.ReadLine();
+            Console.Write($"Digite o endereço do aluno {contador}: ");
+            Adress = Console.ReadLine();
+            Console.WriteLine($"{Name} matriculado");
+        }
+        public void RemoveStudent(string userInputName)
+        {
+            if (userInputName == Name)
+            {
+                Console.WriteLine($"Clique ENTER para expulsar {Name}");
+                Console.ReadKey();
+                Console.WriteLine($"{Name} expulso");
+                Name = null;
+            }
+            else
+            {
+            }
+        }
+        public int Status()
+        {
+            Process = 0;
+            Random random = new Random();
+            Random rando1 = new Random();
+
+            Process = (random.Next(10) + random.Next(10)) / 2;
+            return Process;
+        }
+    }
+}
---
+++ TrabalhoMatricula/TrabalhoMatricula/Program.cs
@@ -0,0 +1,100 @@
+﻿using TrabalhoMatricula;
+
+int index = 10;
+Aluno[] aluno = new Aluno[index];
+
+for (int i = 0; i < index; i++)
+{
+    aluno[i] = new Aluno();
+}
+
+aluno[0].Name = "Fernando";
+aluno[0].MotherName = "Joseline";
+aluno[0].Phone = "47 991463256";
+aluno[0].DateBorn = "26/02/2004";
+aluno[0].ActualState = "SC";
+aluno[0].Adress = "Rua Bahia,355";
+
+Console.WriteLine($"Seja bem vindo a escola E.E Frederico Hardt\n");
+bool userUsing = true;
+while (userUsing)
+{
+    Console.WriteLine("1 - Cadastrar aluno\n2- Expulsar aluno\n3 - Checar estado do aluno\n4 - Encerrar\n");
+    Console.Write("Digite sua escolha: ");
+    var userInputChoice = Console.ReadLine();
+    if (userInputChoice == "1")
+    {
+        int contador = 1;
+        Console.Clear();
+        Console.WriteLine("Quantos alunos deseja cadastrar? Limite de 9");
+        int userInputQtd = Convert.ToInt32(Console.ReadLine());
+        if (userInputQtd > 9)
+        {
+            Console.WriteLine("Não é permitido colocar mais de 9");
+        }
+        else
+        {
+            for (int i = 0; i < userInputQtd; i++)
+            {
+                if (aluno[i].Name == null)
+                {
+                    aluno[i].StudentRegister(contador);
+                    contador++;
+                }
+                else
+                {
+                    userInputQtd++;
+                }
+            }
+        }
+    }
+    if (userInputChoice == "2")
+    {
+        int contador = 1;
+        Console.Clear();
+        Console.WriteLine("Digite o nome do aluno para expulsar");
+        for (int i = 0; i < aluno.Length; i++)
+        {
+            if (aluno[i].Name == null)
+            {
+            }
+            else
+            {
+                Console.WriteLine($"{contador} - Aluno: {aluno[i].Name} Mãe: {aluno[i].MotherName}");
+                contador++;
+            }
+        }
+        string userInputStdChoice = Console.ReadLine();
+        for (int i = 0; i < aluno.Length; i++)
+        {
+            if (aluno[i].Name == null)
+            {
+            }
+            else
+            {
+                aluno[i].RemoveStudent(userInputStdChoice);
+            }
+        }
+    }
+    if (userInputChoice == "3")
+    {
+        Console.Clear();
+        for (int i = 0; i < aluno.Length; i++)
+        {
+            if (aluno[i].Name == null)
+            {
+            }
+            else
+            {
+                Console.WriteLine($"Nome: {aluno[i].Name} Nota: {aluno[i].Process = aluno[i].Status()}");
+            }
+            Thread.Sleep(250);
+        }
+    }
+    if (userInputChoice == "4")
+    {
+        Thread.Sleep(350);
+        Console.WriteLine("Encerrando...");
+        userUsing = false;
+    }
+}
